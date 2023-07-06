60519d944b4924ee9231fcb02eb4d4fab67d3b35
Mark Wanjau <theafrikanson@gmail.com>
2023-07-06 18:12:35 +0300

    complete assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,31 @@
 class Post < ApplicationRecord
+
+
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }
+
+    validate :clickbait_title
+
+    private
+
+    def clickbait_title
+
+    return unless title
+
+    clickbait_phrases = [
+      "Won't Believe",
+      "Secret",
+      "Top [number]",
+      "Guess"
+    ]
+
+    unless clickbait_phrases.any? { |phrase| title.include?(phrase) }
+      errors.add(:title, "must be clickbait-y")
+    end
+
+    
+    end
+
 end
